f8355139c02cef75fcd750dcaf2909f1e91eba1d
mrqdat <33796325+mrqdat@users.noreply.github.com>
2024-12-20 17:01:46 +0700

    change input format, cors

--- MathComapare/Controllers/MathcomparisionController.cs
+++ MathComapare/Controllers/MathcomparisionController.cs
@@ -1,5 +1,8 @@
+using AspNetCoreRateLimit;
 using MathComapare.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MathComapare.Controllers
 {
@@ -8,22 +11,27 @@ namespace MathComapare.Controllers
     public class MathcomparisionController : ControllerBase
     {
         private readonly IMathExpressionService _service;
+        private readonly ILogger<MathcomparisionController> _logger;
 
-        public MathcomparisionController(IMathExpressionService service)
+        public MathcomparisionController(IMathExpressionService service, ILogger<MathcomparisionController> logger)
         {
             _service = service;
+            _logger = logger;
         }
 
         [HttpGet("generate")]
-        public async Task<IActionResult> Generate([FromQuery] string difficulty)
+        //[ServiceFilter(ralimitfiler)]
+        public async Task<IActionResult> Generate([FromQuery] int difficulty)
         {
             try
             {
                 var data = await _service.GenerateExpressions(difficulty);
+                _logger.LogInformation("{ data }", data);
                 return Ok(data);
             }
             catch (Exception ex)
             {
+                _logger.LogWarning(ex.Message);
                 return BadRequest(ex.Message);
             }
         }
@@ -34,10 +42,12 @@ namespace MathComapare.Controllers
             try
             {
                 var valid = await _service.EvaluateComparison(request);
+                _logger.LogInformation("{ data }", valid);
                 return Ok(valid);
             }
             catch (Exception ex)
             {
+                _logger.LogWarning(ex.Message);
                 return BadRequest(ex.Message);
             }
         }
--- MathComapare/Program.cs
+++ MathComapare/Program.cs
@@ -1,4 +1,7 @@
 using MathComapare.Models;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using AspNetCoreRateLimit;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +12,65 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IMathExpressionService, MathComparisonService>();
-var app = builder.Build();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddLogging(option =>
+{
+    option.AddConsole();
+    option.AddDebug();
+});
 
+builder.Services.AddLogging();
+//builder.WebHost.ConfigureKestrel(option =>
+//{
+//    option.ListenAnyIP(5000, listenOption =>
+//    {
+//        listenOption.Protocols = HttpProtocols.Http1AndHttp2;
+//    });
+
+//    option.ListenAnyIP(5001, listenoption =>
+//    {
+//        listenoption.UseHttps();
+//        listenoption.Protocols = HttpProtocols.Http2;
+//    });
+//}); 
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(options =>
+{
+    options.EnableEndpointRateLimiting = true;
+    options.StackBlockedRequests = false;
+    options.RealIpHeader = "X-Real-IP"; 
+    options.GeneralRules = new List<RateLimitRule>
+    {
+        new()
+        {
+            Period = "1s",
+            Limit = 10,
+            Endpoint = "*"
+        }
+    };
+    options.HttpStatusCode = 429;
+    options.QuotaExceededResponse = new QuotaExceededResponse
+    {
+        Content = "{\"message\": \"Too many requests. Please try again later.\"}",
+        ContentType = "application/json",
+        StatusCode = 429
+    };
+});
+
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddCors( option =>
+{
+    option.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000", "https://checkmath.io.vn/")
+        .AllowCredentials()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -18,6 +78,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseIpRateLimiting();
+app.UseRouting();
+app.UseCors("AllowSpecificOrigins");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
